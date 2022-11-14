67c5bffe3c29ad25a9136a51ee7dee12eb52433d
alvaro-ipx <remberto.molina@ipxserver.com>
2022-11-14 12:17:13 -0400

    Setup init

---
+++ README.md
@@ -0,0 +1,3 @@
+# WHATSAPP CLOUD API PACKAGE v1.0.0
+
+## Package for sending Whatsapp messages with the Whatsapp Cloud Api service.
---
+++ src/WhatsappCloudapiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EmizorIpx\WhatsappCloudapi;
+
+use Illuminate\Support\ServiceProvider;
+
+class WhatsappCloudapiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
